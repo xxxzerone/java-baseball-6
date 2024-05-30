480f3f0fee1a89c91dc62f83f5f5b79bb4e67b36
xxxzerone <enpunk90@gmail.com>
2024-05-30 10:17:54 +0900

    feat: 비교 결과값 객체로 구성

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Referee {
 
     // TODO 플레이어와 컴퓨터 수 비교
-    public static String compare(Computer computer, Player player) {
+    public static Result compare(Computer computer, Player player) {
         int strike = 0;
         int ball = 0;
 
@@ -20,19 +20,20 @@ public class Referee {
             }
         }
 
-        // TODO 비교 결과 출력 임시용(나중에 리팩토링)
-        String message = "";
-        if (strike == 3) {
-            message = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-        } else if (strike > 0 && ball == 0) {
-            message = strike + "스트라이크";
-        } else if (ball > 0 && strike == 0) {
-            message = ball + "볼";
-        } else if (strike == 0 && ball == 0) {
-            message = "낫싱";
-        } else {
-            message = ball + "볼 " + strike + "스트라이크";
-        }
-        return message;
+        return new Result(strike, ball);
+
+//        String message = "";
+//        if (strike == 3) {
+//            message = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+//        } else if (strike > 0 && ball == 0) {
+//            message = strike + "스트라이크";
+//        } else if (ball > 0 && strike == 0) {
+//            message = ball + "볼";
+//        } else if (strike == 0 && ball == 0) {
+//            message = "낫싱";
+//        } else {
+//            message = ball + "볼 " + strike + "스트라이크";
+//        }
+//        return message;
     }
 }
